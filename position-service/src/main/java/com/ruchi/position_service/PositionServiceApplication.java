e0eb6a07875520de96ca375d372ceb278f5ebc42
ruchi <ruchi@Linux-5.11.0-25>
2026-08-24 18:05:19 +0530

    chore: initialize position service

---
+++ position-service/src/main/java/com/ruchi/position_service/PositionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ruchi.position_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionServiceApplication.class, args);
+	}
+
+}
