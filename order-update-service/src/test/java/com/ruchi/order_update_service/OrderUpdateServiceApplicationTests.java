f7db9b89ea160db22c141cabf3f92e69f3f850bf
ruchi <ruchi@Linux-5.11.0-25>
2026-08-31 12:49:39 +0530

    chore: initialize order update service

---
+++ order-update-service/src/test/java/com/ruchi/order_update_service/OrderUpdateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruchi.order_update_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderUpdateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
